0c8c8504deb601944e67b5068bd52345c62ffd30
Alexander Gadzhalov <alexander.gadzhalov@limechain.tech>
2023-10-02 06:47:09 +0300

    Added: testMint5KTokensEachUser

--- test/erc20-1/AGTokenTest.t.sol
+++ test/erc20-1/AGTokenTest.t.sol
@@ -9,8 +9,12 @@ contract AGTokenTest is Test {
     AGToken public ag;
 
     address public OWNER = makeAddr("owner");
+    address public USER_ONE = makeAddr("userOne");
+    address public USER_TWO = makeAddr("userTwo");
+    address public USER_THREE = makeAddr("userThree");
 
-    uint256 private constant ONE_HUNDRED_TOKENS = 100000;
+    uint256 private constant ONE_HUNDRED_THOUSAND_TOKENS = 100000;
+    uint256 private constant FIVE_THOUSAND_TOKENS = 5000;
 
     function setUp() public {
         // 1. Deploy your contract from the deployer account
@@ -22,10 +26,23 @@ contract AGTokenTest is Test {
     // 2. Mint 100K tokens to yourself (Deployer)
     function testMint100KTokenToDeployer() public {
         vm.startPrank(OWNER);
-        ag.mint(OWNER, ONE_HUNDRED_TOKENS);
+        ag.mint(OWNER, ONE_HUNDRED_THOUSAND_TOKENS);
         vm.stopPrank();
         console.log("total amount: ", ag.balanceOf(OWNER));
-        assertEq(ag.balanceOf(OWNER), ONE_HUNDRED_TOKENS);
+        assertEq(ag.balanceOf(OWNER), ONE_HUNDRED_THOUSAND_TOKENS);
     }
 
+    //3. Mint 5K tokens to each one of the users
+    function testMint5KTokensEachUser() public {
+        vm.startPrank(OWNER);
+        ag.mint(USER_ONE, FIVE_THOUSAND_TOKENS);
+        assertEq(ag.balanceOf(USER_ONE), FIVE_THOUSAND_TOKENS);
+
+        ag.mint(USER_TWO, FIVE_THOUSAND_TOKENS);
+        assertEq(ag.balanceOf(USER_TWO), FIVE_THOUSAND_TOKENS);
+
+        ag.mint(USER_THREE, FIVE_THOUSAND_TOKENS);
+        assertEq(ag.balanceOf(USER_THREE), FIVE_THOUSAND_TOKENS);
+        vm.stopPrank();
+    }
 }
