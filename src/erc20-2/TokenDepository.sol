24cbf67d4f51a9204a823ef311b23e99843fd740
Alexander Gadzhalov <alexander.gadzhalov@limechain.tech>
2023-10-02 22:48:49 +0300

    Added: deployment of receipt tokens

--- src/erc20-2/TokenDepository.sol
+++ src/erc20-2/TokenDepository.sol
@@ -10,9 +10,12 @@ import {rToken} from "./rToken.sol";
  * @author JohnnyTime (https://smartcontractshacking.com)
  */
 contract TokensDepository {
-
-
     mapping(address => IERC20) public tokens;
+    mapping(address => rToken) public receiptTokens;
+
+    rToken public rAave;
+    rToken public rUni;
+    rToken public rWeth;
     
     // TODO: Complete this contract functionality
     constructor(address _aave, address _uni, address _weth) {
@@ -20,5 +23,10 @@ contract TokensDepository {
         tokens[_aave] = IERC20(_aave);
         tokens[_uni] = IERC20(_uni);
         tokens[_weth] = IERC20(_weth);
+
+        // deploy receipt tokens
+        receiptTokens[_aave] = new rToken(_aave, "receipt Aave", "rAave");
+        receiptTokens[_uni] = new rToken(_aave, "receipt uni", "rUni");
+        receiptTokens[_weth] = new rToken(_aave, "receipt weth", "rWeth");
     }
 }
