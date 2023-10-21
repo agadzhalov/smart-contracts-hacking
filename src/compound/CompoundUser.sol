77ac1d3e31d84b372c1235281ba13684103a3ba7
Alexander Gadzhalov <alexander.gadzhalov@limechain.tech>
2023-10-21 12:21:43 +0300

    CompoundEX: solution by myself, test by Johnny

--- src/compound/CompoundUser.sol
+++ src/compound/CompoundUser.sol
@@ -2,6 +2,7 @@
 // https://smartcontractshacking.com/#copyright-policy
 pragma solidity ^0.8.13;
 
+import {console} from "forge-std/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./CompoundInterfaces.sol";
 
@@ -18,49 +19,64 @@ contract CompoundUser is Ownable {
     uint256 public depositedAmount; // In USDC
     uint256 public borrowedAmount; // In DAI
 
+    address[] private cTokens;
+
     // TODO: Implement the constructor
     constructor(address _comptroller, address _cUsdc, address _cDai) {
-
+        
         // TODO: Set the comptroller, cUsdc, and cDai contracts
+        comptroller = IComptroller(_comptroller);
+        cUsdc = cERC20(_cUsdc);
+        cDai = cERC20(_cDai);
 
         // TODO: Set the usdc, and dai contract (retrieve from cToken contracts)
-
+        usdc = IERC20(cUsdc.underlying());
+        dai = IERC20(cDai.underlying());
     }
 
     // Deposit USDC to Compound
     function deposit(uint256 _amount) external onlyOwner {
         // TODO: Implement this function
 
+        require(usdc.balanceOf(msg.sender) >= _amount, "insufficient balance");
         // TODO: Update depositedAmount state var
+        depositedAmount += _amount;
         
         // TODO: Transfer the USDC from the user to this smart contract
+        usdc.transferFrom(msg.sender, address(this), _amount);
 
         // TODO: Approve the cUsdc contract to spend our USDC tokens
+        usdc.approve(address(cUsdc), _amount);
 
         // TODO: Deposit USDC tokens (mint cUSDC tokens)
-
+        cUsdc.mint(_amount);
     }
 
     // Allow the deposited USDC to be used as collateral, interact with the Comptroller contract
     function allowUSDCAsCollateral() external onlyOwner {
         // TODO: Implement this function
+        cTokens.push(address(cUsdc));
 
         // TODO: Use the comptroller `enterMarkets` function to set the usdc as collateral
-
+        uint[] memory code = comptroller.enterMarkets(cTokens);
+        console.log(code[0]); // 0 = success
     }
 
     // Withdraw deposited USDC from Compound
     function withdraw(uint256 _amount) external onlyOwner {
         // TODO: Implement this function
-
+        
         // TODO: Revert if the user is trying to withdraw more than he deposited
+        require(depositedAmount >= _amount, "too much");
 
         // TODO: Update depositedAmount amount state var
+        depositedAmount -= _amount;
 
         // TODO: Withdraw the USDC tokens
+        cUsdc.redeemUnderlying(_amount);
 
         // TODO: Transfer USDC token to the user
-
+        usdc.transfer(msg.sender, _amount);
     }
 
     // Borrow DAI from Compound
@@ -68,11 +84,13 @@ contract CompoundUser is Ownable {
         // TODO: Implement this function
         
         // TODO: Update borrowedAmount state var
+        borrowedAmount += _amount;
 
         // TODO: Borrow DAI
-        
-        // TODO: Send DAI to the user
+        cDai.borrow(_amount);
 
+        // TODO: Send DAI to the user
+        dai.transfer(msg.sender, _amount);
     }
 
     // Repay the borrowed DAI
@@ -80,14 +98,19 @@ contract CompoundUser is Ownable {
         // TODO: Implement this function
 
         // TODO: Revert if the user is trying to repay more tokens than he borrowed
+        require(borrowedAmount >= _amount, "borrowed less");
 
         // TODO: Update borrowedAmount state var
+        borrowedAmount -= _amount;
 
         // TODO: Transfer the DAI tokens from the user to this contract
+        dai.transferFrom(msg.sender, address(this), _amount);
 
         // TODO: Approve Compound cToken contract to spend the DAI tokens
+        dai.approve(address(cDai), _amount);
         
         // TODO: Repay the loan
-
+        cDai.repayBorrow(_amount);
+        
     }
 }
