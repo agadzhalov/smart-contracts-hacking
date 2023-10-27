b63fb55383802c81135662f09b1ce8f81b3a26a0
Alexander Gadzhalov <alexander.gadzhalov@limechain.tech>
2023-10-27 23:14:27 +0300

    FlashLoan2: impl for only one asset woring

--- src/flash-loan-2/FlashLoan.sol
+++ src/flash-loan-2/FlashLoan.sol
@@ -20,7 +20,20 @@ contract FlashLoan {
 
     // TODO: Implement this function
     function getFlashLoan(address token, uint amount) external {
+        address[] memory assets = new address[](1);
+        assets[0] = token;
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = amount; 
+        uint256[] memory interestRateModes = new uint256[](1);
+        interestRateModes[0] = 0; 
 
+        console.log("Pool before: ", IERC20(token).balanceOf(address(pool)));
+        console.log("Contract before: ", IERC20(token).balanceOf(address(this)));
+
+        pool.flashLoan(address(this),assets,amounts,interestRateModes,address(0),"",0);
+
+        console.log("Pool after: ", IERC20(token).balanceOf(address(pool)));
+        console.log("Contract after: ", IERC20(token).balanceOf(address(this)));
     }
 
     // TODO: Implement this function
@@ -32,5 +45,15 @@ contract FlashLoan {
         bytes memory params
     ) public returns (bool) {
 
+        console.log("Pool during", IERC20(assets[0]).balanceOf(address(pool)));
+        console.log("Contract during: ", IERC20(assets[0]).balanceOf(address(this)));
+
+        // @TODO add for loop to iterate over all assets and amount
+
+        address usdc = assets[0];
+        uint256 amount = amounts[0];
+        uint toBeReturned = amount + premiums[0]; // premius[0] is the fee
+        IERC20(usdc).approve(address(pool), toBeReturned);
+        return true;
     }
 }
--- test/flash-loan-2/FL2Test.t.sol
+++ test/flash-loan-2/FL2Test.t.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../../src/interfaces/ILendingPool.sol";
 import {FlashLoan} from "src/flash-loan-2/FlashLoan.sol";
 
 /**
@@ -13,31 +14,36 @@ contract FL2Test is Test {
 
     address deployer = makeAddr("deployer");
 
-    uint256 constant HUNDRED_ETH = 100 ether;
+    uint256 constant HUNDRED_USDC = 100e6; // 100 USDC
     address public immutable USDC_ADDRESS = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
     address public immutable AAVE_LP_ADDRESS = address(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9);
     address public immutable WHALE_ADDRESS = address(0x8e5dEdeAEb2EC54d0508973a0Fccd1754586974A);
 
     FlashLoan flashLoan;
-    IERC721 usdc;
+    IERC20 usdc;
 
     function setUp() public {
-        uint256 mainnetFork = vm.createFork("");
+        uint256 mainnetFork = vm.createFork("https://eth-mainnet.g.alchemy.com/v2/EHvsIT5Aaj9GF6LX4s1Q6O_2PtVclUk1");
         vm.selectFork(mainnetFork);
         vm.rollFork(15969633);
-        usdc = IERC721(USDC_ADDRESS);
+        usdc = IERC20(USDC_ADDRESS);
 
-        vm.deal(deployer, HUNDRED_ETH);
+        vm.deal(deployer, 100 ether);
 
         vm.prank(deployer);
         flashLoan = new FlashLoan(AAVE_LP_ADDRESS);
-        
-        vm.prank(WHALE_ADDRESS);
-        console.log(usdc.balanceOf(WHALE_ADDRESS));
     }
 
     function testAaveInteraction() public {
-        
+        uint256 poolInitBalance = IERC20(USDC_ADDRESS).balanceOf(AAVE_LP_ADDRESS);
+        vm.startPrank(WHALE_ADDRESS);
+        usdc.transfer(address(flashLoan), HUNDRED_USDC);
+        uint256 contractInitBalance = IERC20(USDC_ADDRESS).balanceOf(address(flashLoan));
+        console.log(contractInitBalance, ILendingPool(AAVE_LP_ADDRESS).FLASHLOAN_PREMIUM_TOTAL() * 1e6);
+        flashLoan.getFlashLoan(USDC_ADDRESS, HUNDRED_USDC);
+        assertTrue(IERC20(USDC_ADDRESS).balanceOf(AAVE_LP_ADDRESS) >= poolInitBalance);
+        assertEq(IERC20(USDC_ADDRESS).balanceOf(address(flashLoan)), contractInitBalance - (ILendingPool(AAVE_LP_ADDRESS).FLASHLOAN_PREMIUM_TOTAL() * 1e6)/100);
+        vm.stopPrank();
     }
 
 
